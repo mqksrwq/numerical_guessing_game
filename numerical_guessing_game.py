2857d637c41ced9659e95b437b648d66eed7dd1c
mqksrwq <maksim4ikb2006@gmail.com>
2025-09-15 17:39:44 +0300

    v1.1 added attempt count

--- numerical_guessing_game.py
+++ numerical_guessing_game.py
@@ -7,16 +7,21 @@ def is_valid(n):
 
 print("Добро пожаловать в числовую угадайку")
 guessed_number = random.randint(1, 100)
+total_attempts = 0
 
 entered_number = int(input("Введите число: "))
 while True:
     if is_valid(entered_number):
         if entered_number < guessed_number:
+            total_attempts += 1
             print("Ваше число меньше загаданного, попробуйте еще разок")
         elif entered_number > guessed_number:
+            total_attempts += 1
             print("Ваше число больше загаданного, попробуйте еще разок")
         else:
+            total_attempts += 1
             print("Вы угадали, поздравляем!")
+            print(f"Итоговое количество попыток: {total_attempts}")
             break
     else:
         print("А может быть все-таки введем целое число от 1 до 100?")
