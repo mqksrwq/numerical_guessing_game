edb30c5f6350c7c08b3f18c4f915f60fd7a58542
mqksrwq <maksim4ikb2006@gmail.com>
2025-09-15 17:51:42 +0300

    v1.2 added the possibility of endless game play

--- numerical_guessing_game.py
+++ numerical_guessing_game.py
@@ -6,25 +6,28 @@ def is_valid(n):
 
 
 print("Добро пожаловать в числовую угадайку")
-guessed_number = random.randint(1, 100)
-total_attempts = 0
-
-entered_number = int(input("Введите число: "))
-while True:
-    if is_valid(entered_number):
-        if entered_number < guessed_number:
-            total_attempts += 1
-            print("Ваше число меньше загаданного, попробуйте еще разок")
-        elif entered_number > guessed_number:
-            total_attempts += 1
-            print("Ваше число больше загаданного, попробуйте еще разок")
-        else:
-            total_attempts += 1
-            print("Вы угадали, поздравляем!")
-            print(f"Итоговое количество попыток: {total_attempts}")
-            break
-    else:
-        print("А может быть все-таки введем целое число от 1 до 100?")
-
+user_answer = "y"
+while user_answer != "n":
+    total_attempts = 0
     entered_number = int(input("Введите число: "))
+    guessed_number = random.randint(1, 100)
+    while True:
+        if is_valid(entered_number):
+            if entered_number < guessed_number:
+                total_attempts += 1
+                print("Ваше число меньше загаданного, попробуйте еще разок")
+            elif entered_number > guessed_number:
+                total_attempts += 1
+                print("Ваше число больше загаданного, попробуйте еще разок")
+            else:
+                total_attempts += 1
+                print("Вы угадали, поздравляем!")
+                print(f"Итоговое количество попыток: {total_attempts}")
+                break
+        else:
+            print("А может быть все-таки введем целое число от 1 до 100?")
+
+        entered_number = int(input("Введите число: "))
+    user_answer = input("Хотите продолжить игру? (y/n): ")
+
 print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
