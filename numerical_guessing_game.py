3ac9f818915f309207c2e8f462e1689ab5f3e9fa
mqksrwq <maksim4ikb2006@gmail.com>
2025-09-15 17:57:58 +0300

    v1.3 added the right edge of the generator range

--- numerical_guessing_game.py
+++ numerical_guessing_game.py
@@ -1,18 +1,22 @@
 import random
 
 
-def is_valid(n):
-    return True if 1 <= n <= 100 else False
+def is_valid(value, n):
+    if 1 <= value <= n:
+        return True
+    else:
+        return False
 
 
 print("Добро пожаловать в числовую угадайку")
 user_answer = "y"
 while user_answer != "n":
     total_attempts = 0
+    right_border = int(input("Введите правую границу диапазона генератора: "))
+    guessed_number = random.randint(1, right_border)
     entered_number = int(input("Введите число: "))
-    guessed_number = random.randint(1, 100)
     while True:
-        if is_valid(entered_number):
+        if is_valid(entered_number, right_border):
             if entered_number < guessed_number:
                 total_attempts += 1
                 print("Ваше число меньше загаданного, попробуйте еще разок")
