e5d4595a2e865952ecc0a75dbc4d3abe736e0801
mqksrwq <maksim4ikb2006@gmail.com>
2025-09-15 19:02:14 +0300

    v1.4 added a check for numbers && changed the question for the new game && added a check for the suggested symbols when trying to play a new game

--- numerical_guessing_game.py
+++ numerical_guessing_game.py
@@ -11,27 +11,41 @@ def is_valid(value, n):
 print("Добро пожаловать в числовую угадайку")
 user_answer = "y"
 while user_answer != "n":
+
     total_attempts = 0
-    right_border = int(input("Введите правую границу диапазона генератора: "))
+    input_line = input("Введите правую границу диапазона генератора: ")
+    if not input_line.isdigit():
+        print("Нужно ввести число")
+        continue
+    else:
+        right_border = int(input_line)
+
     guessed_number = random.randint(1, right_border)
-    entered_number = int(input("Введите число: "))
+
     while True:
+        input_line = input("Введите число: ")
+        if not input_line.isdigit():
+            print("Нужно ввести число")
+            continue
+        else:
+            entered_number = int(input_line)
+            total_attempts += 1
+
         if is_valid(entered_number, right_border):
             if entered_number < guessed_number:
-                total_attempts += 1
                 print("Ваше число меньше загаданного, попробуйте еще разок")
             elif entered_number > guessed_number:
-                total_attempts += 1
                 print("Ваше число больше загаданного, попробуйте еще разок")
             else:
-                total_attempts += 1
                 print("Вы угадали, поздравляем!")
                 print(f"Итоговое количество попыток: {total_attempts}")
                 break
         else:
-            print(f"А может быть все-таки введем целое число от 1 до {right_border}?")
+            print(f"Введите целое число от 1 до {right_border}?")
 
-        entered_number = int(input("Введите число: "))
-    user_answer = input("Хотите продолжить игру? (y/n): ")
+    user_answer = input("Хотите начать игру заново? (y/n): ")
+    while user_answer not in "yn":
+        print("Введите предложенные символы")
+        user_answer = input("Хотите начать игру заново? (y/n): ")
 
 print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
